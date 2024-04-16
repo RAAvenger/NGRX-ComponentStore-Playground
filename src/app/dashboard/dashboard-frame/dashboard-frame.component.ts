ce93ccdf39290766e554a3781a422146b554fe46
Reza Adibi <adbrezaooo@gmail.com>
2024-04-16 12:10:54 +0330

    added parameter input

--- src/app/dashboard/dashboard-frame/dashboard-frame.component.ts
+++ src/app/dashboard/dashboard-frame/dashboard-frame.component.ts
@@ -1,14 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { FrameData } from './dtos/frame-data';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard-frame',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './dashboard-frame.component.html',
   styleUrl: './dashboard-frame.component.scss',
 })
 export class DashboardFrameComponent {
   @Input({ required: true })
   public frameData!: FrameData;
+  protected parameterLabel: string = '';
+
+  protected changeParameterLabel(newLabel: string) {
+    this.parameterLabel = newLabel;
+  }
 }
